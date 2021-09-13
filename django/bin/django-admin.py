7637812f39489cc3157bb450c5dabe3eedb61c65
SpastBanana <sebastiaan.j.d.v@outlook.com>
2021-09-13 16:52:31 +0200

    I created Django

--- django/bin/django-admin.py
+++ django/bin/django-admin.py
@@ -1,4 +1,4 @@
-#!/home/sebastiaan/Desktop/H2Heating/django/bin/python
+#!/home/pi/Desktop/H2Heating/django/bin/python3
 # When the django-admin.py deprecation ends, remove this script.
 import warnings
 
