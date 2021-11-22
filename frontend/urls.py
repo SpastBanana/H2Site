534ff934784a576375dee11371cf31ada281d3c3
Nigel3005 <57489967+Nigel3005@users.noreply.github.com>
2021-11-22 17:11:43 +0100

    log-in

--- frontend/urls.py
+++ frontend/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.indexView, name='Home'),
-    path('login', views.loginView, name="Login"),
+    path('log-in', views.loginView, name="Log-in"),
     path('logout', views.logoutView, name="Logout"),
     path('register', views.registerView, name="Register"),
     path('profiel', views.profielView, name='Profiel'),
