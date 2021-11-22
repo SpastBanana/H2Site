128fafa6355c5598a986fd0cdae04c84cc88de70
Nigel3005 <57489967+Nigel3005@users.noreply.github.com>
2021-11-22 16:10:33 +0100

    changed settings.py

--- H2Site/settings.py
+++ H2Site/settings.py
@@ -131,18 +131,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 # SECURE_SSL_REDIRECT = True
 LOGIN_REDIRECT_URL = '/home'
 LOGIN_URL = '/login'
 
 # LOCAL STATIC
-STATICFILES_DIRS = (str(BASE_DIR.joinpath('static/')),) # new
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles/") # new
+STATICFILES_DIRS = (str(BASE_DIR.joinpath('static')),) # new
+STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles')) # new
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage' # new
-
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
